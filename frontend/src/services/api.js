9e4cc92ae725d2f8ffc35e4e0be35eb018584a0a
BhanuPraksh2227 <alakuntlabhanuprakash@gmail.com>
2025-09-16 11:22:01 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000', // Removed /api prefix
+    baseURL: 'https://orphan-oldagehomes-donation-1.onrender.com', // Removed /api prefix
     headers: {
         'Content-Type': 'application/json'
     }
